884e154911e4037ac795b56ee5eabc76af559972
Hassan Lakhal <hlakhal-@e1r3p3.1337.ma>
2023-07-26 23:43:10 +0100

    add ex00

--- main.h
+++ main.h
@@ -5,6 +5,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <readline/history.h>
+#include <readline/readline.h>
+#include <signal.h>
+#include <sys/stat.h>
 
 
 #endif
